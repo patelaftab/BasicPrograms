23a933044f037b125d4b4e049b4e3ef78f02d4c0
patelaftab <10patelaftab@gmail.com>
2023-02-06 09:10:36 +0530

    [Aftab] : Checking Head And tail Percentage

---
+++ BasicProblems/FlipCoin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems
+{
+    public class FlipCoin
+    {
+        public  void CoinFlip()
+        {
+            Console.WriteLine("Enter a how many Time you Want to Flip The Coin");
+            int flipnum=Convert.ToInt32(Console.ReadLine());
+            int Head = 0, Tail = 0;
+            Random random = new Random();
+            for(int i=0;i<flipnum;i++)
+            {
+                double numcheck=random.NextDouble();
+                if (numcheck>0.5)
+                {
+                    Head++;
+                }
+                else 
+                {
+                    Tail++;
+                }
+                
+            }
+            double Headpercentage = Head * 100 / flipnum;
+            double Tailpercentage = Tail * 100 / flipnum;
+            Console.WriteLine("Head Percentage Is :" + Headpercentage);
+            Console.WriteLine("Tail percentage Is :" + Tailpercentage);
+        }
+    }
+}
---
+++ BasicProblems/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Basic Core Programs");
+            Console.WriteLine("Choose a Option \n" +
+                "1: FlipCoin Problem");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option) 
+            {
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.CoinFlip();
+                    break;
+                default:
+                    Console.WriteLine("Choose a Correct option");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
